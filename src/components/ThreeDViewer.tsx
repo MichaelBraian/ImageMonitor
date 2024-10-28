cff6f54b4921fb2aa088e7fd8be331afa66133cb
MichaelBraian <ddsmichael@gmail.com>
2024-10-28 20:01:24 +0100

    3D wiever orthograpich

--- src/components/ThreeDViewer.tsx
+++ src/components/ThreeDViewer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { OrbitControls, OrthographicCamera } from '@react-three/drei';
 import { Model } from './Model';
 
 interface ThreeDViewerProps {
@@ -12,8 +12,22 @@ export function ThreeDViewer({ fileUrl, fileFormat }: ThreeDViewerProps) {
   return (
     <div className="w-full h-full">
       <Canvas>
-        <PerspectiveCamera makeDefault position={[0, 0, 10]} />
-        <OrbitControls makeDefault enableRotate={true} enableZoom={true} enablePan={true} />
+        <OrthographicCamera
+          makeDefault
+          zoom={50}
+          position={[0, 0, 100]}
+          near={0.1}
+          far={2000}
+        />
+        <OrbitControls 
+          makeDefault 
+          enableRotate={true} 
+          enableZoom={true} 
+          enablePan={true}
+          // Add these props for better orthographic control
+          minZoom={10}
+          maxZoom={500}
+        />
         <ambientLight intensity={0.9} />
         <directionalLight position={[10, 10, 5]} intensity={0.5} />
         <Model url={fileUrl} format={fileFormat} />
